13daac339e9142e2ce0c98fcca41c22560fcb811
Anna <anna-katharina.dick@student.uni-tuebingen.de>
2022-03-22 14:24:01 +0100

    weird error appeared

--- Run_model.py
+++ Run_model.py
@@ -1,14 +1,16 @@
 import Bias_model
 
-# config
 csvpath = 'combined_descriptions.csv'
 outputpath = 'movie_model_w2v'
-clusteroutputpath = 'gender_clusters.txt'
+clusteroutputpath = 'clusters/adjective_clusters.txt'
+
+#noun_list = ['NN', 'NNP', 'NNS'] #for nltk
+adjective_list = ['JJ','JJR','JJS'] #for nltk
+noun_list = ['NOUN'] #for spacy
 
 '''
 Train new model
 '''
-#Bias_model.label_clusters(['hello'], ['you'])
 
 print('Training new model', csvpath)
 Bias_model.TrainModel(csvpath,
@@ -32,20 +34,14 @@ print('Finding biases...')
 female = ['female', 'woman', 'girl', 'mother', 'daughter', 'sister', 'she', 'her', 'hers']
 male = ['male', 'man', 'boy', 'son', 'father', 'brother', 'he', 'him', 'his']
 
-other = ["non binary", "transgender", "trans", "transwoman", "transsexual"]
-
-muslim = ["allah", "ramadan", "turban", "emir", "salaam", "sunni", "koran",
-          "imam", "sultan", "prophet", "veil", "ayatollah", "shiite", "mosque", "islam", "sheik", "muslim", "muhammad",
-          'islamic']
-christian = ["baptism", "messiah", "catholicism", "resurrection", "christianity", "salvation", "protestant", "gospel",
-             "trinity", "jesus", "christ", "christian", "cross", "catholic", "church"]
 #Bias_model.getMostSimilarWords(outputpath, female, male)
 [b1, b2] = Bias_model.GetTopMostBiasedWords(
     outputpath,  # model path
-    200,  # topk biased words
+    100,  # topk biased words
     female,  # target set 1
     male,  # target set 2
-    ['JJ', 'JJR', 'JJS'],  # nltk pos to be considered #'JJ','JJR','JJS',
+    adjective_list, #pos to be considered
+    use_spacy=False,
     verbose=False)
 
 print('Biased words towards ', female)
@@ -62,7 +58,7 @@ print('Clustering words into concepts...')
 [cl1, cl2] = Bias_model.Cluster(
     b1,  # set of words biased towards target set 1
     b2,  # set of words biased towards target set 2
-    0.15,  # r 0.15
+    0.3,  # r 0.15
     10,  # repeat
     verbose=False)
 
@@ -75,25 +71,7 @@ print('Clusters biased towards ', male)
 for cluster in cl2:
     print([k['word'] for k in cluster])
 
-print('Printing to file...')
-with open(clusteroutputpath, 'wt') as f:
-    f.write('#Clusters for %s' % female)
-    f.write('\n')
-    for cluster in cl1:
-        cluster_list = [k['word'] for k in cluster]
-        for word in cluster_list:
-            f.write(word + ' ')
-        f.write('\n')
-    f.write('\n')
-    f.write('#Clusters for %s' % male)
-    f.write('\n')
-    for cluster in cl2:
-        cluster_list = [k['word'] for k in cluster]
-        for word in cluster_list:
-            f.write(word + ' ')
-        f.write('\n')
-print()
-print('*Finished')
-
-print()
-print('*Finished')
+'''
+print words in clusters to file
+'''
+#Bias_model.print_to_file(clusteroutputpath, male, female, cl1, cl2)
